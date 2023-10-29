d4f5516d3962f58376e13131e4ae1f6dc6f12d72
Achim Winter <achim.winter95@gmail.com>
2023-10-29 21:12:30 +0100

    Remove longhorn

--- terraform/config/main.tf
+++ terraform/config/main.tf
@@ -5,25 +5,8 @@ module "jmusicbot" {
   vault_id       = var.vault_id
 }
 
-# module "longhorn" {
-#   source = "./modules/longhorn"
-
-#   compartment_id = var.compartment_id
-#   vault_id       = var.vault_id
-# }
-
 module "traefik" {
   source  = "./modules/traefik"
 
   compartment_id = var.compartment_id
 }
-
-# module "certmanager" {
-#   source = "./modules/cert-manager"
-# }
-
-# module "externaldns" {
-#   source = "./modules/external-dns"
-
-#   compartment_id = var.compartment_id
-# }
