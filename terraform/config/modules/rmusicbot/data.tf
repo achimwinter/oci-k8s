e361d7e601ece55eff65c571c9b4a73494050fe9
Achim Winter <achim.winter95@gmail.com>
2023-12-03 22:31:49 +0100

    Add rmusicbot

---
+++ terraform/config/modules/rmusicbot/data.tf
@@ -0,0 +1,3 @@
+data "oci_secrets_secretbundle" "rmusic_token" {
+  secret_id = "ocid1.vaultsecret.oc1.eu-frankfurt-1.amaaaaaaugy2z4yapk5vjedaeepzzf665wsrortgscotttliqc6aznej35dq"
+}
---
+++ terraform/config/modules/rmusicbot/variables.tf
@@ -0,0 +1,9 @@
+variable "vault_id" {
+  description = "OCI Vault OIDC"
+  type        = string
+}
+
+variable "compartment_id" {
+  description = "The compartment to create the resources in"
+  type        = string
+}
