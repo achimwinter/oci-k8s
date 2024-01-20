26affd433e966d37292654786bb68823e6e3515a
Achim Winter <achim.winter95@gmail.com>
2024-01-20 16:12:05 +0100

    Update postgres

--- terraform/config/modules/postgres/helm.tf
+++ terraform/config/modules/postgres/helm.tf
@@ -2,7 +2,7 @@ resource "helm_release" "postgresql" {
   chart      = "postgresql"
   name       = "postgresql"
   repository = "https://charts.bitnami.com/bitnami"
-  version    = "13.2.27"
+  version    = "13.2.29"
   namespace  = "postgresql"
 
   create_namespace = true
