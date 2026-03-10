49bea448fccfa41d8fb1b996a908b111350b356a
Achim Winter <achim.winter95@gmail.com>
2026-03-10 10:32:13 +0100

    Update nextcloud

--- terraform/config/modules/nextcloud/helm.tf
+++ terraform/config/modules/nextcloud/helm.tf
@@ -2,7 +2,7 @@ resource "helm_release" "nextcloud" {
   chart      = "nextcloud"
   name       = "nextcloud"
   repository = "https://nextcloud.github.io/helm/"
-  version    = "8.9.1"
+  version    = "9.0.3"
   namespace  = "nextcloud"
 
   create_namespace = true
